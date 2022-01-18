ea9aa0cb85b22a28a1e1ddf698c2b963c3fed07c
Dmitrij Kolesnev <darklight1985@mail.ru>
2022-01-18 23:26:42 +0300

    1.1. Состояние нити. [#229175 #252510] исправил 4

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -8,16 +8,16 @@ public class ThreadState {
         Thread second = new Thread(
                 () -> System.out.println(Thread.currentThread().getName())
         );
-        System.out.println(first.getState());
-        System.out.println(second.getState());
+        System.out.println(first.getName() + " " + first.getState());
+        System.out.println(second.getName() + " " + second.getState());
         first.start();
         second.start();
-        while (first.getState() != Thread.State.TERMINATED) {
+        while (first.getState() != Thread.State.TERMINATED && second.getState() != Thread.State.TERMINATED) {
             System.out.println(first.getName() + " " + first.getState());
-        }
-        while (second.getState() != Thread.State.TERMINATED) {
             System.out.println(second.getName() + " " + second.getState());
         }
         System.out.println("Работа завершена");
+        System.out.println(first.getName() + " " + first.getState());
+        System.out.println(second.getName() + " " + second.getState());
     }
 }
