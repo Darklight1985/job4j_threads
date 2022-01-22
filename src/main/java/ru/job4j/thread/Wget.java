fc11f695a4b423eafe7535af5b27191e38a45e6b
Dmitrij Kolesnev <darklight1985@mail.ru>
2022-01-22 19:04:20 +0300

    4. Скачивание файла с ограничением. [#144271 #253596] исправил 3

--- src/main/java/ru/job4j/thread/Wget.java
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -10,17 +10,19 @@ import java.time.Instant;
 public class Wget implements Runnable {
     private final String url;
     private final int speed;
+    private final String dest;
 
-    public Wget(String url, int speed) {
+    public Wget(String url, int speed, String dest) {
         this.url = url;
         this.speed = speed;
+        this.dest = dest;
     }
 
     @Override
     public void run() {
 
         try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-             FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
+             FileOutputStream fileOutputStream = new FileOutputStream(dest)) {
             byte[] dataBuffer = new byte[1024];
             int bytesRead;
             Instant start = Instant.now();
@@ -32,11 +34,11 @@ public class Wget implements Runnable {
                     long elapsed = Duration.between(start, Instant.now()).toMillis();
                     System.out.println("Скачано " + bytesWrited
                             + " байт за " + elapsed + " миллисекунд");
+                    start = Instant.now();
+                    bytesWrited = 0;
                     if (elapsed < 1000) {
                         System.out.println("Need pause in " + (1000 - elapsed) + " ms");
                         Thread.sleep(1000 - elapsed);
-                        start = Instant.now();
-                        bytesWrited = 0;
                     }
                 }
             }
@@ -47,10 +49,18 @@ public class Wget implements Runnable {
         }
     }
 
+    private static void parse(String[] args) {
+            if (args.length < 3) {
+                throw new IllegalArgumentException();
+        }
+    }
+
     public static void main(String[] args) throws InterruptedException {
+        parse(args);
         String url = args[0];
         int speed = Integer.parseInt(args[1]);
-        Thread wget = new Thread(new Wget(url, speed));
+        String dest = args[2];
+        Thread wget = new Thread(new Wget(url, speed, dest));
         wget.start();
         wget.join();
     }
