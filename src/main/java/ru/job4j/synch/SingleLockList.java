d6901703fdb389220d62342fe873848a7a137f93
Dmitrij Kolesnev <darklight1985@mail.ru>
2022-01-26 21:16:33 +0300

    4. ThreadSafe динамический список [#1105]

---
+++ src/main/java/ru/job4j/synch/SingleLockList.java
@@ -0,0 +1,36 @@
+package ru.job4j.synch;
+
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+@ThreadSafe
+public class SingleLockList<T> implements Iterable<T> {
+    private final List<T> list;
+
+    public SingleLockList(List<T> list) {
+        this.list = Collections.synchronizedList(list);
+    }
+
+    public void add(T value) {
+        list.add(value);
+    }
+
+    public T get(int index) {
+        return list.get(index);
+    }
+
+    public synchronized List copy(List<T> list) {
+        List<T> tList = new ArrayList<>(list);
+        Collections.copy(tList, list);
+        return tList;
+    }
+
+    @Override
+    public synchronized Iterator<T> iterator() {
+        return copy(list).iterator();
+    }
+}
