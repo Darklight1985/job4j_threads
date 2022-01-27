40afe8ad708cc48b2654d2f3c1edee0cde6d02ce
Dmitrij Kolesnev <darklight1985@mail.ru>
2022-01-27 09:09:38 +0300

    4. ThreadSafe динамический список [#1105 #256235] исправил

--- src/main/java/ru/job4j/synch/SingleLockList.java
+++ src/main/java/ru/job4j/synch/SingleLockList.java
@@ -12,14 +12,14 @@ public class SingleLockList<T> implements Iterable<T> {
     private final List<T> list;
 
     public SingleLockList(List<T> list) {
-        this.list = Collections.synchronizedList(list);
+        this.list = list;
     }
 
-    public void add(T value) {
+    public synchronized void add(T value) {
         list.add(value);
     }
 
-    public T get(int index) {
+    public synchronized T get(int index) {
         return list.get(index);
     }
 
