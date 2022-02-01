20b0e06ce8a7ad5b21d62a8e698762fdb73ceecd
Dmitrij Kolesnev <darklight1985@mail.ru>
2022-02-01 21:12:03 +0300

    2. ExecutorService рассылка почты. [#63097] исправил

--- src/main/java/ru/job4j/pool/EmailNotification.java
+++ src/main/java/ru/job4j/pool/EmailNotification.java
@@ -14,14 +14,19 @@ public class EmailNotification {
 
      public void emailTo(User user) {
          pool.submit(() -> {
-            String subject = "Notification {" + user.getUsername() + "} to email {" + user.getEmail() + "}";
-             String body = "Add a new event to {" + user.getUsername() + "}";
-
+            String subject = "Notification {"
+                    + user.getUsername()
+                    + "} to email {"
+                    + user.getEmail() + "}";
+             String body = "Add a new event to {"
+                     + user.getUsername() + "}";
              send(subject, body, user.getEmail());
          });
      }
 
-     public void send(String subject, String body, String email) {}
+     public void send(String subject, String body, String email) {
+
+     }
 
     public void close() {
          pool.shutdown();
@@ -33,6 +38,4 @@ public class EmailNotification {
              }
          }
      }
-
-
 }
