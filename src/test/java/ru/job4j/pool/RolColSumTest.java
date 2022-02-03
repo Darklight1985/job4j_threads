5c4752778d31143f43cd431a478d754fb26e5d52
Dmitrij Kolesnev <darklight1985@mail.ru>
2022-02-03 20:07:50 +0300

    4. CompletableFuture [#361626] исправил2

--- src/test/java/ru/job4j/pool/RolColSumTest.java
+++ src/test/java/ru/job4j/pool/RolColSumTest.java
@@ -10,7 +10,7 @@ public class RolColSumTest {
 
     @Test
     public void sumOne() {
-        int[][] matrix = new int[][]{{1, 2, 3,}, {4, 5, 6}, {7, 8, 9},};
+        int[][] matrix = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
         RolColSum.Sums[] sum = RolColSum.sum(matrix);
        int rsl = sum[1].getColSum();
        assertEquals(rsl, 15);
@@ -18,7 +18,7 @@ public class RolColSumTest {
 
     @Test
     public void sumTwo() {
-        int[][] matrix = new int[][]{{1, 2, 3,}, {4, 5, 6}, {7, 8, 9},};
+        int[][] matrix = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
         RolColSum.Sums[] sum = RolColSum.sum(matrix);
         int rsl = sum[2].getRowSum();
         assertEquals(rsl, 24);
@@ -26,7 +26,7 @@ public class RolColSumTest {
 
     @Test
     public void sumAsyncOne() throws ExecutionException, InterruptedException {
-        int[][] matrix = new int[][]{{1, 2, 3,}, {4, 5, 6}, {7, 8, 9},};
+        int[][] matrix = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
         RolColSum.Sums[] sum = RolColSum.asyncSum(matrix);
         int rsl = sum[2].getRowSum();
         assertEquals(rsl, 24);
@@ -34,7 +34,7 @@ public class RolColSumTest {
 
     @Test
     public void sumAsyncTwo() {
-        int[][] matrix = new int[][]{{1, 2, 3,}, {4, 5, 6}, {7, 8, 9},};
+        int[][] matrix = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
         RolColSum.Sums[] sum = RolColSum.sum(matrix);
         int rsl = sum[0].getColSum();
         assertEquals(rsl, 12);
