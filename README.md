7e554b05d0b65388eb5c8645f09cb350b5cef3d8
Dmitrij Kolesnev <darklight1985@mail.ru>
2022-02-06 11:51:26 +0300

    3.2. Откат изменений. [#307144]

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 Educational project. Contains exercises for passing the Middle module in the course from job4j
 
 You can contact me at the postal address darklight1985@mail.ru
-
-Измненения добавил
---
+++ src/main/java/ru/job4j/git/Myclass.java
@@ -0,0 +1,9 @@
+package ru.job4j.git;
+
+public class Myclass {
+    public static void main(String[] args) {
+        int chislo = 1 + 2;
+        System.out.println("Что то посчитали " + chislo);
+        System.out.println("Что то выводим");
+    }
+}
