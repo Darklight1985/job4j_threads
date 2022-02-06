103d7f3de59332199982936cb35c99ac28de7c05
Dmitrij Kolesnev <darklight1985@mail.ru>
2022-02-06 22:26:25 +0300

    локальные изменения

--- src/main/java/ru/job4j/git/Myclass.java
+++ src/main/java/ru/job4j/git/Myclass.java
@@ -4,6 +4,5 @@ public class Myclass {
     public static void main(String[] args) {
         int chislo = 1 + 2;
         System.out.println("Что то посчитали " + chislo);
-        System.out.println("Что то выводим");
     }
 }
